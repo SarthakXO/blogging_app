e0dcce098522a2670419387bf22f9be0f04ee58a
SarthakXO <sarthakbhatt@live.com>
2024-04-09 17:13:24 +0530

    created blog card

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,14 @@
+import BlogCard from "@/components/BlogCard";
+
 const page = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="grid grid-cols-4 gap-6 mt-4 p-4">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
+  );
 };
 
 export default page;
---
+++ components/BlogCard.tsx
@@ -0,0 +1,29 @@
+const BlogCard = () => {
+  return (
+    <div>
+      <div className="max-w-sm overflow-hidden shadow-lg border border-gray-700 rounded-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-200 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
