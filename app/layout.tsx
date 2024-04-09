8cf35c92e735d47c01bccd7964067ba68700c473
SarthakXO <sarthakbhatt@live.com>
2024-04-09 10:47:56 +0530

    created links and pages for blogs, about, contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,22 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="sticky bg-red-400">
-      <div className="flex">
-        <div>Title</div>
-        <div>Links</div>
+    <nav className="sticky bg-black p-4">
+      <div className="flex justify-around text-xl text-white">
+        <Link href={"/"}>
+          <div>Title</div>
+        </Link>
+        <div className="flex gap-14">
+          <Link href={"/blogs"}>
+            <div>Blogs</div>
+          </Link>
+          <Link href={"/about"}>
+            <div>About</div>
+          </Link>
+          <Link href={"/contact"}>
+            <div>Contact</div>
+          </Link>
+        </div>
       </div>
     </nav>
   );
