9cdbe9959d5b2649317715e4d66e663876f2b24c
SarthakXO <sarthakbhatt@live.com>
2024-04-09 10:41:43 +0530

    created navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky bg-red-400">
+      <div className="flex">
+        <div>Title</div>
+        <div>Links</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
