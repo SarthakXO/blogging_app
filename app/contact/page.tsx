3ee9bf5ef00aec9adc6c3d2327bb9646ad0b3983
SarthakXO <sarthakbhatt@live.com>
2024-04-09 16:08:56 +0530

    made contact us form into client component but page renders on server

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,49 +1,11 @@
-"use client";
-import { Formik, Field, ErrorMessage, FormikProps, Form } from "formik";
+import ContactUsForm from "@/components/ContactUsForm";
+
 const page = () => {
   return (
     <div className="flex justify-center items-center text-center ">
-      <div className="w-[600px] h-[600px] mt-10 border border-gray-800 rounded-xl p-8">
+      <div className="w-[600px] h-[800px] mt-10 border border-gray-800 object-contain rounded-xl p-8">
         <h1 className="text-3xl">Contact Us</h1>
-        <Formik
-          initialValues={{ name: " ", email: " ", phone: " " }}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
-          <Form className="text-white mt-10 grid grid-cols-1 gap-10">
-            <div className="flex gap-6">
-              <label>Name : </label>
-              <div> </div>
-              <Field
-                className="bg-black w-[400px] text-white border border-gray-800 hover:border-gray-500 duration-300 rounded-lg ml-4"
-                name="name"
-                type="text"
-              ></Field>
-            </div>
-
-            <div className="flex gap-6">
-              <label>Email:</label>
-              <div> </div>
-              <Field
-                className="bg-black w-[400px] text-white border border-gray-800 hover:border-gray-500 duration-300 rounded-lg ml-4"
-                name="email"
-                type="text"
-              ></Field>
-            </div>
-
-            <div className="flex gap-6">
-              <label>Phone:</label>
-              <div> </div>
-              <Field
-                className="bg-black w-[400px] text-white border border-gray-800 hover:border-gray-500 duration-300 rounded-lg ml-4"
-                name="phone"
-                type="text"
-              ></Field>
-            </div>
-            <button type="submit">Submit</button>
-          </Form>
-        </Formik>
+        <ContactUsForm />
       </div>
     </div>
   );
---
+++ components/ContactUsForm.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { Formik, Field, ErrorMessage, FormikProps, Form } from "formik";
+import * as Yup from "yup";
+const ContactUsForm = () => {
+  const contactSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("Name is required")
+      .min(3, "Name is too short")
+      .max(50, "Name is too Long"),
+    email: Yup.string().email("Invalid email").required(),
+    phone: Yup.string()
+      .min(10, "Invalid Number")
+      .max(14, "Invalid Number")
+      .required("This is required"),
+  });
+  return (
+    <div>
+      <Formik
+        initialValues={{ name: " ", email: " ", phone: " " }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={contactSchema}
+      >
+        <Form className="text-white mt-10 grid grid-cols-1 gap-20">
+          <div className="flex gap-6">
+            <label htmlFor="name">Name:</label>
+            <div> </div>
+            <Field
+              className="bg-black w-full text-white border hover:scale-105 border-gray-800 hover:border-gray-500 duration-300 rounded-lg ml-4"
+              name="name"
+              type="text"
+            />
+          </div>
+          <ErrorMessage name="name" />
+
+          <div className="flex gap-6">
+            <label>Email:</label>
+            <div> </div>
+            <Field
+              className="bg-black w-full text-white border hover:scale-105 border-gray-800 hover:border-gray-500 duration-300 rounded-lg ml-4"
+              name="email"
+              type="text"
+            />
+          </div>
+          <ErrorMessage name="email" />
+
+          <div className="flex gap-6">
+            <label>Phone:</label>
+            <div> </div>
+            <Field
+              className="bg-black w-full text-white border border-gray-800 hover:scale-105 hover:border-gray-500 duration-300 rounded-lg ml-4"
+              name="phone"
+              type="text"
+            />
+          </div>
+          <ErrorMessage className="text-white" name="phone" />
+          <div className="flex justify-center">
+            <button
+              className="border border-gray-800 w-[200px] rounded-lg hover:scale-105 hover:bg-slate-700 duration-300 "
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+          <span className="text-gray-500 ">
+            We Will reach out to you within 5-10 business days
+          </span>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default ContactUsForm;
