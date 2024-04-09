00868b1d4ebd802c77e35b011b547380024aaf66
SarthakXO <sarthakbhatt@live.com>
2024-04-09 17:09:36 +0530

    Landing Page made

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,127 @@
+import Link from "next/link";
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')]">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+          {/* <!-- Announcement Banner --> */}
+          <div className="flex justify-center">
+            <Link
+              className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-200"
+              href="mailto:sarthakbhatt@live.com"
+            >
+              To Hire Sarthak - Join to waitlist
+              <span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-gray-700 dark:text-gray-400">
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </Link>
+          </div>
+          {/* <!-- End Announcement Banner --> */}
+
+          {/* <!-- Title --> */}
+          <div className="mt-5 max-w-2xl text-center mx-auto">
+            <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+              Let&apos;s Build
+              <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+                {" "}
+                Together
+              </span>
+            </h1>
+          </div>
+          {/* <!-- End Title --> */}
+
+          <div className="mt-5 max-w-3xl text-center mx-auto">
+            <p className="text-lg text-gray-600 dark:text-gray-400">
+              Blogger App made by SarthakXO, takes a mock API and uses it to
+              display cards with their Title, Body and Tags. The cards are
+              rendered on the server for better SEO and optimization.
+            </p>
+          </div>
+
+          {/* <!-- Buttons --> */}
+          <div className="mt-8 gap-3 flex justify-center">
+            <Link
+              className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
+              href="http://localhost:3000/blogs"
+            >
+              Check Out the blogs
+              <svg
+                className="flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6" />
+              </svg>
+            </Link>
+          </div>
+          {/* <!-- End Buttons --> */}
+
+          <div className="mt-5 flex justify-center items-center gap-x-1 sm:gap-x-3">
+            <span className="text-sm text-gray-600 dark:text-gray-400">
+              Full Stack Web Developer:
+            </span>
+            <span className="text-sm font-bold text-gray-900 dark:text-white">
+              Next.JS/React, TypeScript, Node.JS
+            </span>
+            <svg
+              className="size-5 text-gray-300 dark:text-gray-600"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                d="M6 13L10 3"
+                stroke="currentColor"
+                strokeLinecap="round"
+              />
+            </svg>
+            <Link
+              className="inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 hover:underline font-medium"
+              href="https://github.com/SarthakXO?tab=overview&from=2024-04-01&to=2024-04-09"
+              target="_blank"
+            >
+              Check My Github
+              <svg
+                className="flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6" />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
