e13dbf9c9965e3926f09e69648ea90784d39589f
SarthakXO <sarthakbhatt@live.com>
2024-04-09 17:38:32 +0530

    added blogs, loader for blogs and api requests

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,28 +1,45 @@
 import BlogCard from "@/components/BlogCard";
 import axios from "axios";
 
+interface Blog {
+  id: Number;
+  title: String;
+  body: String;
+  userId: Number;
+  tags: String[];
+  reactions: Number;
+}
+
 const fetchData = async () => {
-  const data = await axios
-    .get("http://localhost:3000/blogs")
+  const reponse = await axios
+    .get("http://localhost:3000/api/blogs")
     .then((res) => res.data);
 
-  return data;
+  console.log(reponse);
+
+  return reponse;
 };
 const page = async () => {
+  const data = await fetchData();
   return (
-    <div className="grid grid-cols-4 gap-6 mt-4 p-4">
-      <div>
-        <BlogCard />
-      </div>
-      <div>
-        <BlogCard />
-      </div>
-      <div>
-        <BlogCard />
-      </div>
-      <div>
-        <BlogCard />
-      </div>
+    <div className="flex flex-row flex-wrap gap-6 mt-4 p-4 justify-center">
+      {data.data.posts.map((blog: Blog) => {
+        return (
+          <div
+            key={`${blog.id}`}
+            className="object-contain hover:scale-105 duration-300"
+          >
+            <BlogCard
+              id={blog.id}
+              title={blog.title}
+              body={blog.body}
+              reactions={blog.reactions}
+              tags={blog.tags}
+              userId={blog.userId}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,25 +1,35 @@
-const BlogCard = () => {
+interface Blog {
+  id: Number;
+  title: String;
+  body: String;
+  userId: Number;
+  tags: String[];
+  reactions: Number;
+}
+
+const BlogCard = (props: Blog) => {
   return (
-    <div>
+    <div className="">
       <div className="max-w-sm overflow-hidden shadow-lg border border-gray-700 rounded-lg">
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <div className="font-bold text-xl mb-2">{props.title}</div>
           <p className="text-gray-200 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+            {`${props.body.slice(0, 350)}..`}
+            <br />
+            <span className="text-blue-500">Know More</span>
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
+            {props.tags[0]}
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
+            {props.tags[1]}
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
+            {props.tags[2]}
           </span>
+          {/* <div className="font-semibold text-lg text-gray-600 mb-2">{props.title}</div> */}
         </div>
       </div>
     </div>
