bfccc21d0ee8915b5d8f280e74462d9b7113b4c8
SarthakXO <sarthakbhatt@live.com>
2024-04-10 20:00:07 +0530

    about us page done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,36 @@
+import Link from "next/link";
+
 const page = () => {
   return (
     <div>
-      <section className="bg-white dark:bg-gray-900">
+      <section className=" bg-black">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">
-              Powering innovation at{" "}
-              <span className="font-extrabold">200,000+</span> companies
-              worldwide
+          <div className="max-w-screen-lg sm:text-lg text-gray-400">
+            <h2 className="mb-4 text-4xl tracking-tight font-bold  text-white">
+              Creating Next.JS applications. <br />
+              <br />
+              <span className="font-extrabold">Sarthak Bhatt,</span> Web
+              developer
             </h2>
             <p className="mb-4 font-light">
-              Track work across the enterprise through an open, collaborative
-              platform. Link issues across Jira and ingest data from other
-              software development tools, so your IT support and operations
-              teams have richer contextual information to rapidly respond to
-              requests, incidents, and changes.
+              With over a year of professional experience and a robust portfolio
+              of personal projects, I stand as a dedicated web developer
+              specializing in Next.js. My journey in the world of web
+              development is marked by a relentless pursuit of innovation and
+              mastery in modern web technologies, with a particular focus on
+              enhancing user experience and optimizing web performance
             </p>
             <p className="mb-4 font-medium">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.Accelerate critical development work,
-              eliminate toil, and deploy changes with ease.
+              My expertise in Next.js, combined with a passion for crafting
+              dynamic and responsive websites, enables me to deliver tailored
+              solutions that meet the unique needs of each project. Through my
+              work, I aim not only to meet but exceed expectations, pushing the
+              boundaries of what is possible on the web.
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700"
+            <Link
+              href="https://github.com/SarthakXO"
+              target="_blank"
+              className="inline-flex items-center font-medium text-blue-600 hover:text-blue-300 duration-300"
             >
               Learn more
               <svg
@@ -38,7 +45,7 @@ const page = () => {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
       </section>
