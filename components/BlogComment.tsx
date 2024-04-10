db7cb1f1f219b198d0488036366b12549952db2b
SarthakXO <sarthakbhatt@live.com>
2024-04-10 14:07:36 +0530

    fixed comments

--- components/BlogComment.tsx
+++ components/BlogComment.tsx
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+interface Comment {
+  id: Number;
+  body: String;
+  postId: Number;
+  user: {
+    id: Number;
+    username: String;
+  };
+}
 const fetchData = async (id: String) => {
   const data = axios
     .get(`http://localhost:3000/api/comments/${id}`)
@@ -8,16 +17,26 @@ const fetchData = async (id: String) => {
 };
 
 const BlogComment = async ({ id }: { id: String }) => {
+  let response;
   try {
-    const response = await fetchData(id).then((res) => res.data);
-    console.log(response);
+    response = await fetchData(id).then((res) => res.data);
   } catch (e) {
     console.log(e);
   }
   return (
-    <div className="flex mt-10 justify-start gap-10 w-1/2 border border-gray-800 h-">
-      <div>Username: </div>
-      <div>Comment </div>
+    <div className="grid grid-cols-1 place-items-center w-full mb-10 pb-4 pt-4">
+      <div className="text-3xl"> COMMENTS</div>
+      {response.comments.map((comment: Comment) => {
+        return (
+          <div
+            className="flex mt-10 justify-start gap-10 w-1/2 items-center  "
+            key={`${comment.id}`}
+          >
+            <div>{comment.user.username}: </div>
+            <div>{comment.body}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
