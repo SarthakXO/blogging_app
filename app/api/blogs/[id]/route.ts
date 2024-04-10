1ff5a83e73da38eb57e100ea6c56606672b2bbe4
SarthakXO <sarthakbhatt@live.com>
2024-04-10 13:04:19 +0530

    hot fix for backend bug

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -4,8 +4,16 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
   const params = req.url;
   const length = params.length;
+  let id = "";
+  for (let i = length; i--; i > 0) {
+    console.log("running");
+    if (params[i] == "/") {
+      break;
+    }
+    id = params[i] + id;
+  }
 
-  const id = params[length - 1];
+  //   const url = req.nextUrl.pathname;
 
   const data = await axios
     .get(`https://dummyjson.com/posts/${id}`)
